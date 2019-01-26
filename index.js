bfccb62e882457b275e343975949d53bd43cccb2
said shah <saidmysr20@aol.com>
2019-01-25 20:33:34 -0500

    added all of the css and html for the page

---
+++ index.js
@@ -0,0 +1,13 @@
+//QUEUE data-structure
+
+// a queue is FIFO => First In First Out
+
+// queue methods are
+
+// isEmpty() => returns whether a queue is empty or not
+
+// getFront() => returns the front of the queue, the first one waiting to get out
+
+// dequeue() => to remove an element from the queue
+
+// enqueue() => to add an element to a queue
